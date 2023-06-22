940de91c29fe63bade04f3669ff8a67e4f64a783
isaias jesus <isaias@casa.com>
2023-06-21 21:18:13 -0300

    primeiro repositorio do projeto

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $('#carrosel-imagens').slick({
+        autoplay: true,
+    });
+
+    $('.menu-hamburguer').click(function(){
+        $('nav').slideToggle();
+    });
+
+    $('#telefone').mask('(00)00000-0000', {  /*adiciona o placeholder exibe info no campo de prenchimento, S em letra e 0 em numeros*/
+        placeholder: '(00)00000-0000'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            VeiculoDeInteresse: {
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu Nome'
+        },
+        submitHandler: function(form) {
+            console.log(form);
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    });
+
+    $('.lista-veiculos button').click(function(){
+        const destino = $('#contato');
+        const nomeVeiculo = $(this).parent().find('h3').text();
+
+        $('#veiculo-de-interesse').val(nomeVeiculo);
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000);
+    });
+    
+});
